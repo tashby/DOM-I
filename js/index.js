1e2ddf7254da1fbacd64f13e2a4bc76d1cac5214
Tashby <tashby2714@gmail.com>
2020-04-09 01:06:38 -0400

    copyright

--- js/index.js
+++ js/index.js
@@ -78,9 +78,12 @@ mainContentParagraphs[3].textContent = siteContent['main-content']['product-cont
 mainContentParagraphs[4].textContent = siteContent['main-content']['vision-content'];
 
 let contactHeader = document.querySelector('.contact h4');
-contactHeader.textContent = siteContent['contact']['contact-h4']
+contactHeader.textContent = siteContent['contact']['contact-h4'];
 
 let contactInfo = document.querySelectorAll('.contact p');
-contactInfo[0].textContent = siteContent['contact']['address']
-contactInfo[1].textContent = siteContent['contact']['phone']
-contactInfo[2].textContent = siteContent['contact']['email']
+contactInfo[0].textContent = siteContent['contact']['address'];
+contactInfo[1].textContent = siteContent['contact']['phone'];
+contactInfo[2].textContent = siteContent['contact']['email'];
+
+let footerCopyright = document.querySelector('footer p');
+footerCopyright.textContent = siteContent['footer']['copyright']
